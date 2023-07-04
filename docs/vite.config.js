af4f6f667927695647213c1d09b3524236d6db46
younghero <yxzhang@hitotek.com>
2023-07-04 17:07:47 +0800

    chore: 增加自动生成组件文件夹脚本 & 修改readme

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 // import vue from '@vitejs/plugin-vue'
-import ElementPlus from 'unplugin-element-plus/vite'
+// import ElementPlus from 'unplugin-element-plus/vite'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
---
+++ scripts/gc.sh
@@ -0,0 +1,63 @@
+#! /bin/bash
+
+NAME=$1
+
+FILE_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")/../packages" && pwd)
+
+re="[[:space:]]+"
+
+if [ "$#" -ne 1 ] || [[ $NAME =~ $re ]] || [ "$NAME" == "" ]; then
+  echo "Usage: pnpm gc \${name} with no space"
+  exit 1
+fi
+
+DIRNAME="$FILE_PATH/components/$NAME"
+DIRNAMESRC="$FILE_PATH/components"
+
+INPUT_NAME=$NAME
+
+if [ -d "$DIRNAME" ]; then
+  echo "$NAME component already exists, please change it"
+  exit 1
+fi
+
+NORMALIZED_NAME=""
+for i in $(echo $NAME | sed 's/[_|-]\([a-z]\)/\ \1/;s/^\([a-z]\)/\ \1/'); do
+  C=$(echo "${i:0:1}" | tr "[:lower:]" "[:upper:]")
+  NORMALIZED_NAME="$NORMALIZED_NAME${C}${i:1}"
+done
+NAME=$NORMALIZED_NAME
+
+mkdir -p "$DIRNAME"
+mkdir -p "$DIRNAME/src"
+
+cat > $DIRNAME/src/$INPUT_NAME.vue <<EOF
+<script setup>
+
+defineOptions({
+  name: 'FZ$NAME',
+})
+
+const props = defineProps({})
+
+// init here
+</script>
+
+<template>
+  <div>
+    <slot />
+  </div>
+</template>
+EOF
+
+cat <<EOF >"$DIRNAME/index.js"
+import { withInstall } from '@hitotek/fuzzy-ui-utils'
+import $NAME from './src/$INPUT_NAME.vue'
+
+export const FZ$NAME = withInstall($NAME)
+export default FZ$NAME
+
+export * from './src/$INPUT_NAME'
+EOF
+
+echo "export * from './$INPUT_NAME/index.js'">>"$DIRNAMESRC/export-components.js"
