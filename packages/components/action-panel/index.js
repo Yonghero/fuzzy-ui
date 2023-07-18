40f195695a17f61b8f2cecd3f4603a625c12f873
younghero <yxzhang@hitotek.com>
2023-07-18 10:53:34 +0800

    chore: base

---
+++ packages/components/action-panel/index.js
@@ -0,0 +1,7 @@
+import { withInstall } from '@hitotek/fuzzy-ui-utils'
+import ActionPanel from './src/ActionPanel.vue'
+
+export const FYActionPanel = withInstall(ActionPanel)
+export default FYActionPanel
+
+export * from './src/ActionPanel.vue'
